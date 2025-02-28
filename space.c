65977b348d5cada76a0b787e8e893088116e7d27
xyztwiki <blackfairyforma726@gmail.com>
2025-02-28 12:50:26 +0800

    Update space.c

--- space.c
+++ space.c
@@ -2,13 +2,6 @@
 #include "abcdef.c"
 #include "DNA.c"
 #include "RNA.c"
-const t1 = 1234567;
-const t2 = 2345678;
-const t3 = 3456789;
-const t4 = 4567890;
-const t5 = 5678901;
-const t6 = 6789012;
-const t7 = 7890123;
 void space(a,b,c,d,e,f){
     char A,B,C,D,E,F;
     return a * (A + t1) + b * (B - t2);
