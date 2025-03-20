6e13ee2e5a2b49c24d448dc3203e9e0ffa5f0d85
xyztwiki <blackfairyforma726@gmail.com>
2025-03-20 21:31:28 +0800

    Update abcdef.c

--- abcdef.c
+++ abcdef.c
@@ -6,11 +6,3 @@ float abcdef(){
     float e = 5 / 7;
     float f = 6 / 7;
 }
-char ABCDEF(){
-    char A = "142857";
-    char B = "285714";
-    char C = "428571";
-    char D = "571428";
-    char E = "857142";
-    char F = "999999"; 
-}
