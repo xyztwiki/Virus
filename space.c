bf58aa176233fc99a11ebf65a29e7179994a7781
xyztwiki <blackfairyforma726@gmail.com>
2025-03-20 21:31:06 +0800

    Update space.c

--- space.c
+++ space.c
@@ -1,11 +1,9 @@
-#include "time.c"
 #include "abcdef.c"
-#include "DNA.c"
-#include "RNA.c"
-void space(a,b,c,d,e,f){
-    char A,B,C,D,E,F;
-    return a * (A + t1) + b * (B - t2);
-    return c * (C + t3) + d * (D - t4);
-    return e * (E + t5) + f * (F - t6);
-    return f * (F + t7);
-} 
+void calculation(){
+    return "a + b + c + d + e + f"
+    return "a - b - c - d - e - f"
+    return "a * b * c * d * e * f"
+    return "a / b / c / d / e / f"
+    return "a % b % c % d % e % f"
+    return "a : b : c : d : e : f"
+}
